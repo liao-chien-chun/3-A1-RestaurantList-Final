57916be8f5a3dad9a2c6ace80caf488feb9c99b8
Eric LIAO <d1044171009@gmail.com>
2022-07-19 14:20:09 +0800

    設定樣版引擎，靜態頁面

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express') //載入express
 const exphbs = require('express-handlebars') //載入handlebars
 
+const port = 3000
 const app = express()
 
+//設定樣版引擎
+//透過app.engine來定義要使用的樣板引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+//透過app.set告訴express說要設定view engine 是handlebars
+app.set('view engine', 'handlebars')
 
-
+//setting static files
+app.use(express.static('public'))
 
 //設置路由
 app.get('/', (req, res) => {
-  res.send('This is a homepage')
+  res.render('index')
 })
 
-app.listen(3000, () => {
-  console.log('server is running on localhost:3000')
+app.listen(port, () => {
+  console.log(`server is running on localhost:${port}`)
 })
 
