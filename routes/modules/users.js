b49c0569015e2e6df80e4c9bf6fc3bfd03e05618
Eric LIAO <d1044171009@gmail.com>
2022-08-25 20:41:21 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,6 +5,16 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+
+
 module.exports = router
 
 
