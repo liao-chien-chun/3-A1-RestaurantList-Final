ec8035fe09f8ba7cf21aa118a98d866e720eff84
Eric LIAO <d1044171009@gmail.com>
2022-08-25 21:06:31 +0800

    feat: add user register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -13,6 +15,33 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+// 表單註冊路由
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  // 檢查是否已註冊
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('此使用者已經存在')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword,
+        })
+      } else {
+        // 還沒註冊寫入資料庫
+        return User.create({
+          name,
+          email,
+          password,
+        })
+          .then(() => res.redirect('/users/login'))
+          .catch(err => console.log(err))
+      }
+    })
+    .catch(err => console.log(err))
+})
 
 
 module.exports = router
